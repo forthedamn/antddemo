7275aadc8cb84fc281bc0fd5f04126634ffaa917
TomJerryTheHandsome <464432079@qq.com>
2021-09-18 16:42:14 +0800

    2 files modified, 2 added

--- src/pages/Auths/index.js
+++ src/pages/Auths/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Card, Breadcrumb, Layout } from "antd";
-
+import React from 'react';
+import { Card, Breadcrumb, Layout } from 'antd';
+import { getAuthList, editAuth, addAuth } from '../../../api/auth/index';
 const { Content } = Layout;
 
 function NewService1() {
@@ -11,7 +11,7 @@ function NewService1() {
         <Breadcrumb.Item href="/services/1">权限管理</Breadcrumb.Item>
       </Breadcrumb>
 
-      <Content style={{ margin: "15px 0" }}>
+      <Content style={{ margin: '15px 0' }}>
         <p>权限管理内容</p>
       </Content>
     </Card>
